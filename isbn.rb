1066e434c8b7675d95df0e1e69d83c4fa51bb0ea
christimrichmond <christimrichmond@gmail.com>
2017-12-13 13:14:20 -0500

    finished project with help

--- isbn.rb
+++ isbn.rb
@@ -1,28 +1,62 @@
 
 puts "Please enter an ISBN number: \n \n"
-user_input = gets.chomp 
+user_input = gets.chomp
 
-
-number_array = user_input.split(' ') {|s| s.to_i}
-number_array.count
-digit_check = user_input
-
-if user_input.length == 10 
-		puts "10 \n"
-		return isbn_ten
-	elsif user_input.length == 13
-		puts "13 \n"
-		#return digit_check13(user_input)
-	else
-		puts "Number is invalid. ISBN not found."
+def isbn_check10(number_array)
+	taken_array = number_array
+	number = 10
+	total = 0
+	taken_array.each do |digits| 
+	total = total + (digits * number)
+	number = number - 1
 	end
 
-ten = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+	if total % 11 == 0
+		puts "This is a valid ten digit ISBN."
+	else 
+		puts "This is NOT a valid 10 digit ISBN."
+	end
 
-p ten.map { |ten| ten ** number_array}
+end
+
+def isbn_check13(number_array)
+	array13= [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1]
+	total = 0
+	number = 0
+	number_array.each do |digits13|
+	total = total + (digits13 * array13[number])
+	number = number + 1
+	end
+
+	if total %10 == 0
+		puts "This is a valid 13 digit ISBN."
+	else 
+		puts "This is NOT a valid 13 digit ISBN."
+	end
+end
+
+number_array = user_input.split("").map(&:to_i)
+
+count = number_array.count
+
+if count == 10 
+	isbn_check10(number_array)
+	elsif user_input.length == 13
+	isbn_check13(number_array)
+	else
+		puts "Number is invalid. ISBN must be 10 or 13 digits."
+	end
+
+
+
 
 
 ## Just junk from here on down ##
+
+# ten = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+# p ten.map { |ten| ten ** number_array}
+
 # puts user_input
 # puts number_array
  #puts user_input.length
@@ -30,4 +64,10 @@ p ten.map { |ten| ten ** number_array}
  #def digit_check(user_input)
  	#puts user_input.length
 	
+#puts count
 
+#digit_check = user_input
+
+
+#p number_array
+#p number_array.class
